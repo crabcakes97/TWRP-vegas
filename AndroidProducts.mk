a53b5d3d2062e7b10af91703da5f5c152047b602
Romlord14495 <eddiebob146@gmail.com>
2026-09-02 04:29:51 -0500

    Update AndroidProducts.mk
    Vegas test

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,12 @@
 #OrangeFox/TWRP Config
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_nevada.mk \
-    $(LOCAL_DIR)/omni_nevada.mk
+    $(LOCAL_DIR)/twrp_vegas.mk \
+    $(LOCAL_DIR)/omni_vegas.mk
 
 COMMON_LUNCH_CHOICES := \
-    twrp_nevada-user \
-    twrp_nevada-userdebug \
-    twrp_nevada-eng \
-    omni_nevada-user \
-    omni_nevada-userdebug \
-    omni_nevada-eng
+    twrp_vegas-user \
+    twrp_vegas-userdebug \
+    twrp_vegas-eng \
+    omni_vegas-user \
+    omni_vegas-userdebug \
+    omni_vegas-eng
